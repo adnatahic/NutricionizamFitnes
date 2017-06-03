ab2b866d2e457eac5c841f236943d6ec2c4a3447
Merima Cisija <mcisija1@etf.unsa.ba>
2017-06-03 11:53:46 +0200

    Korisnici modul

---
+++ Fit&Nut/KorisniciModul/src/main/java/com/korisnici/security/TokenAuthenticatioService.java
@@ -0,0 +1,136 @@
+package com.korisnici.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security
+            .authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import com.korisnici.module.Administrator;
+import com.korisnici.module.Korisnik;
+import com.korisnici.module.Osoba;
+import com.korisnici.module.Trener;
+import com.korisnici.repository.AdministratorRepository;
+import com.korisnici.repository.KorisnikRepository;
+import com.korisnici.repository.OsobaRepository;
+import com.korisnici.repository.TrenerRepository;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Collections.emptyList;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Component
+public class TokenAuthenticatioService {
+	
+	static OsobaRepository or;
+	static AdministratorRepository ar;
+	static KorisnikRepository kr;
+	static TrenerRepository tr;
+	
+	@Autowired
+	public TokenAuthenticatioService(OsobaRepository or, AdministratorRepository ar, KorisnikRepository kr, TrenerRepository tr)
+	{
+		TokenAuthenticatioService.or = or;
+		TokenAuthenticatioService.ar = ar;
+		TokenAuthenticatioService.kr = kr;
+		TokenAuthenticatioService.tr = tr;
+	}
+	
+  static final long EXPIRATIONTIME = 864_000_000; // 10 days
+  static final String SECRET = "ThisIsASecret";
+  static final String TOKEN_PREFIX = "Bearer";
+  static final String HEADER_STRING = "Authorization";
+  
+    
+
+  static void addAuthentication(HttpServletResponse res, String username) {
+	  
+	  Osoba o = or.findByUsername(username);
+	  int idOsobe = o.getId();
+	  
+	  List<Administrator> admini = (List<Administrator>) ar.findAll();
+      List<Korisnik> korisnici = (List<Korisnik>) kr.findAll();
+      List<Trener> treneri = (List<Trener>) tr.findAll();
+      
+      String rola = "";
+      
+      
+      for(int i = 0; i < admini.size(); i++)
+      {
+      	if(admini.get(i).getOsoba().getId() == idOsobe) 
+      	{
+      		rola = "ADMIN";
+      	}
+      }
+      
+      for(int i = 0; i < korisnici.size(); i++)
+      {
+      	if(korisnici.get(i).getOsoba().getId() == idOsobe)
+      	{
+      		rola = "KORISNIK";
+      	}
+      }
+      
+      for(int i = 0; i < treneri.size(); i++)
+      {
+      	if(treneri.get(i).getOsoba().getId() == idOsobe)
+      	{
+      		rola = "TRENER";
+      	}
+      }
+	  
+    String JWT = Jwts.builder()
+        .setSubject(username).claim("authority", rola)
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+        .signWith(SignatureAlgorithm.HS512, SECRET)
+        .compact();
+    res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+  }
+
+  static Authentication getAuthentication(HttpServletRequest request) {
+    String token = request.getHeader(HEADER_STRING);
+    if (token != null) {
+      // parse the token.
+      String user = Jwts.parser()
+          .setSigningKey(SECRET)
+          .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+          .getBody()
+          .getSubject();
+      
+      String rola= Jwts.parser()
+				.setSigningKey(SECRET)
+				.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+				.getBody()
+				.get("authority").toString();
+      
+      System.out.println(rola);
+		
+		Collection<GrantedAuthority> prava = new HashSet<GrantedAuthority>();
+		prava.add(new SimpleGrantedAuthority(rola));
+
+      return user != null ?
+          new UsernamePasswordAuthenticationToken(user, null, prava) :
+          null;
+    }
+    return null;
+  }
+}
