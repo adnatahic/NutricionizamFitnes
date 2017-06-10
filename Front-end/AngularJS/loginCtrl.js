7b69dc9d00bec586f0c38ed54eb3842e0bb92ea9
Merima Cisija <mcisija1@etf.unsa.ba>
2017-06-10 16:09:22 +0200

    Nesto
    nn

---
+++ Front-end/AngularJS/loginCtrl.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('NutricionizamFitnes');
+
+app.controller('loginCtrl', ['$window', '$scope', '$http', function ($window, $scope, $http) {
+    $scope.message = "";
+    $scope.login = function () {
+        //alert($scope.username + " " + $scope.password);
+        $http({
+            method: 'POST',
+            url: '/login',
+            contentType: "application/json",
+            data: angular.toJson({ username: $scope.username, password: $scope.password }, true)
+
+        }).success(function (data) {
+            console.log(data);
+            $scope.message = data;
+            $window.location.href = '/korisnici/index.html';
+        }).error(function (error) {
+            console.log(error);
+            alert("Pogresan username i/ili password!");
+        });
+
+    };
+
+}]);
