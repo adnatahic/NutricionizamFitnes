d8abf7eae8be2364e5e83f6a799ecfb85fd92a85
Merima Cisija <mcisija1@etf.unsa.ba>
2017-06-18 12:39:21 +0200

    Treneri

--- Front-end/AngularJS/treneri/treneriCtrl.js
+++ Front-end/AngularJS/treneri/treneriCtrl.js
@@ -2,37 +2,71 @@
     var NutricionizamFitnes = angular.module('NutricionizamFitnes');
 
     NutricionizamFitnes.controller('treneriCtrl', ['$scope', '$http', '$localStorage', 'treneriServis', function ($scope, $http, $localStorage, treneriServis) {
-        var odabrani = null;
-        var odabraniTrener = null;
+
         var serviceBase = "http://localhost:8081/";
         $scope.listaTrenera = [];
         $scope.listaOsoba = [];
 
+        $scope.trener = {
+            id: "",
+            brojKlijenata: "",
+            edukacija: "",
+            godine: "",
+            iskustvo: "",
+            spol: "",
+            osoba: {
+                id: "",
+                email: "",
+                ime: "",
+                password: "",
+                prezime: "",
+                username: ""
+            }
+        };
+
+        $scope.odabraniTrener = {
+            id: "",
+            brojKlijenata: "",
+            edukacija: "",
+            godine: "",
+            iskustvo: "",
+            spol: "",
+            osoba: {
+                id: "",
+                email: "",
+                ime: "",
+                password: "",
+                prezime: "",
+                username: ""
+            }
+        };
+
         $scope.selektujTrenera = function (trener) {
-            odabraniTrener = trener;
-            console.log(odabraniTrener);
+            $scope.odabraniTrener = trener;
         }
 
         $scope.otkaziTrener = function () {
-            odabraniTrener = null;
+            console.log($scope.odabraniTrener);
+            $scope.odabraniTrener = null;
+            console.log($scope.odabraniTrener);
         }
 
         $scope.selektuj = function (korisnik) {
-            odabrani = korisnik;
+            $scope.odabrani = korisnik;
         }
-     
+
         $scope.otkazi = function (korisnik) {
-            odabrani = null;
+            $scope.odabrani = null;
         }
-        
-        
+
+
         $scope.izlistajSveOsobe = function () {
-            console.log($localStorage.nesto);
+            console.log($localStorage.token);
             var urlSveOsobe = serviceBase + 'korisnici/osobe/svi';
 
             $http({
                 method: 'GET', url: urlSveOsobe, headers: {
-                    'Authorization': $localStorage.nesto,
+                    'Authorization': $localStorage.token,
                 }
             })
         .success(function (data) {
@@ -41,12 +75,12 @@
         };
 
         $scope.izlistajSveTrenere = function () {
-            console.log($localStorage.nesto);
+            console.log($localStorage.token);
             var urlSviTreneri = serviceBase + 'korisnici/treneri/svi';
 
             $http({
                 method: 'GET', url: urlSviTreneri, headers: {
-                    'Authorization': $localStorage.nesto,
+                    'Authorization': $localStorage.token,
                 }
             })
         .success(function (data) {
@@ -55,18 +89,42 @@
         };
 
         $scope.dodajTrenera = function (spol, godine, edukacija, iskustvo) {
-            console.log($localStorage.nesto);
-            var urlDodajTrenera = serviceBase + 'korisnici/treneri/dodaj/' + spol +'/'+ godine + '/' + edukacija + '/' + iskustvo + '/0/' + odabrani.id;
-            console.log(urlDodajTrenera);
-            $http({
-                method: 'GET', url: urlDodajTrenera, headers: {
-                    'Authorization': $localStorage.nesto,
-                }
+            console.log($localStorage.token);
+            if (godine >= 18 && iskustvo > 0) {
+                var urlDodajTrenera = serviceBase + 'korisnici/treneri/dodaj/' + spol + '/' + godine + '/' + edukacija + '/' + iskustvo + '/0/' + odabrani.id;
+                console.log(urlDodajTrenera);
+                $http({
+                    method: 'GET', url: urlDodajTrenera, headers: {
+                        'Authorization': $localStorage.token,
+                    }
+                })
+            .success(function (data) {
+                $scope.message = "Trener uspješno dodan!";
             })
-        .success(function (data) {
-            $scope.message = "Trener uspješno dodan!";
-        })
+            }
+            else alert("Trener mora biti punoljetan.\nIskustvo mora biti pozitivan broj.");
         };
 
-        }]);
-    }());
+        $scope.izmijeniTrenera = function (trener) {
+            $scope.trenerRandom = trener;
+            console.log($localStorage.token);
+            if (godine >= 18 && iskustvo > 0) {
+                var urlIzmijeniTrenera = serviceBase + '/treneri/update/' + $scope.trenerRandom.id + '/' + $scope.trenerRandom.spol
+                    + '/' + $scope.trenerRandom.godine + '/' + $scope.trenerRandom.edukacija +
+                    '/' + $scope.trenerRandom.iskustvo + '/' + $scope.trenerRandom.broj_klijenta + '/'
+                    + $scope.trenerRandom.osoba.id;
+                console.log(urlIzmijeniTrenera);
+                $http({
+                    method: 'GET', url: urlIzmijeniTrenera, headers: {
+                        'Authorization': $localStorage.token,
+                    }
+                })
+            .success(function (data) {
+                $scope.message = "Trener uspješno izmijenjen!";
+            })
+            }
+            else alert("Trener mora biti punoljetan.\nIskustvo mora biti pozitivan broj.");
+        };
+
+    }]);
+}());
